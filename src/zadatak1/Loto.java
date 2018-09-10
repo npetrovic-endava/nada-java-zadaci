9e38ea11f07e8fe64ca881802a0efc8929617e0d
Nadezda Petrovic <nadezda.petrovic@endava.com>
2018-09-10 13:34:41 +0200

    vec odobreni zadaci loto i digitron

--- src/zadatak1/Loto.java
+++ src/zadatak1/Loto.java
@@ -1,61 +1,71 @@
+/*
+ * Korisnik preko standardnog ulaza unosi jedan ceo broj u opsegu 1-50. Broj koji treba pogoditi je  
+ * generisan pomocu Random klase. U zavisnosti od rezultata pogadjanja (razlika dva broja veca, manja ili jednaka 5)
+ * ispisuje se odgovarajuca poruka
+ */
 package zadatak1;
+
 import java.util.Random;
 import java.util.Scanner;
 
-
-
+/**
+ *
+ * @author Nadezda Petrovic
+ */
 public class Loto {
-	static Scanner ulaz = new Scanner(System.in);
-	public static void main(String[] args) {
 
-		while (true) {
+    static Scanner ulaz = new Scanner(System.in);
 
-			
-			Random r = new Random();
-			int slucajanBroj = r.nextInt(50) + 1;
-			StringBuilder sb = new StringBuilder("Trazeni broj je ");
-			System.out.println("Unesite ceo broj do 1-50:");
+    public static void main(String[] args) {
+        Random random = new Random();
+        int slucajanBroj = random.nextInt(50) + 1;
+        StringBuilder stringBuild = new StringBuilder("Trazeni broj je: ");
+        boolean pomocna=true;
+        while (pomocna) {
+            
+            System.out.println("Unesite ceo broj do 1-50:");
 
-			// ispituje da li je unet ispravan format
-			if (ulaz.hasNextInt()) {
-				int unetiBroj = ulaz.nextInt();
+            // ispituje da li je unet ispravan format
+            int unetiBroj = proveraInt();
+            // ispituje interval vrednosti
+            if (51 > unetiBroj && 0 < unetiBroj) {
+                // ispituje uspesnost u pogadjanju broja
+                if (slucajanBroj == unetiBroj) {
+                    System.out.println("Neverovatno!Pogodili ste tacan broj!");
+                } else if (5 >= Math.abs(unetiBroj - slucajanBroj)) {
 
-				// ispituje interval vrednosti
-				if (51 > unetiBroj && 0 < unetiBroj) {
+                    System.out.println("Dobar pokusaj, bili ste jako blizu!");
 
-					// ispituje uspesnost u pogadjanju broja
-					if (slucajanBroj == unetiBroj)
-						System.out.println("Neverovatno!Pogodili ste tacan broj!");
+                } else {
+                    System.out.println("Vise srece drugi put!");
+                }
 
-					else if (5 >= Math.abs(unetiBroj - slucajanBroj)) {
-						
-						System.out.println(sb.append(slucajanBroj).append(". Dobar pokusaj, bili ste jako blizu!"));
-						
-					} else
-						System.out.println(sb.append(slucajanBroj).append(", vise srece drugi put!"));
-			
+                System.out.println(stringBuild);
+                System.out.println(slucajanBroj);
+                
+                System.out.println("Da li zelite da nastavite sa programom (da ili ne)?");
+                String nastavak = ulaz.next();
+                if (nastavak.equalsIgnoreCase("da")) {
+                    slucajanBroj = random.nextInt(50) + 1;
+                    pomocna=true;
+                } else {
+                    System.out.println("Kraj!");
+                    pomocna=false;
+                }
+            } else {
+                System.out.println("Pogresan interval, uneti broj treba da bude u intervalu 1-50!");
+                pomocna=true;
+            }
 
-					System.out.println("Da li zelite da nastavite sa programom (da ili ne)?");
-					String nastavak = ulaz.next();
-					if (nastavak.equalsIgnoreCase("da"))
-						continue;
-					else {
-						System.out.println("Kraj!");
-						break;
-					}
+        }
+        ulaz.close();
+    }
 
-				} else {
-					System.out.println("Pogresan interval, uneti broj treba da bude u intervalu 1-50!");
-					continue;
-				}
-
-			} else {
-				System.out.println("Pogresan format, unesite ceo broj!");
-				continue;
-			}
-			
-		}
-		ulaz.close();
-	}
-
+    public static int proveraInt() {
+        while (!ulaz.hasNextInt()) {
+            System.out.println("Pogresan parametar, unesite ceo broj!");
+            ulaz.next();
+        }
+        return ulaz.nextInt();
+    }
 }
--- src/zadatak2/Digitron.java
+++ src/zadatak2/Digitron.java
@@ -1,74 +1,93 @@
+/*
+ * Korisnik unosi dva broja, zatim kod operacije koju zeli da izvrsi 
+ * (s-sabiranje, o-oduzimanje, m-mnozenje, d-deljenje)
+ * Rezultat je ispisivanje rezultata operacije nad operandima
+ */
 package zadatak2;
+
+/**
+ *
+ * @author Nadezda Petrovic
+ */
 import javax.swing.JOptionPane;
 
 public class Digitron {
 
-	public static void main(String[] args) {
-		double rezultat = 0;
-		int broj1;
-		int broj2;
+    public static void main(String[] args) {
+        double rezultat;
+        double broj1;
+        double broj2;
+        boolean pomocna1;
+        boolean pomocna2;
+        boolean f = false;
 
-		while (true) {
-			String s1 = JOptionPane.showInputDialog("Unesite prvi broj sa kojim zelite da radite:");
+        while (true) {
+            String s1 = JOptionPane.showInputDialog("Unesite prvi broj sa kojim zelite da radite:");
 
-			String s2 = JOptionPane.showInputDialog("Unesite drugi broj sa kojim zelite da radite:");
+            String s2 = JOptionPane.showInputDialog("Unesite drugi broj sa kojim zelite da radite:");
 
-			// ispituje da li je unet prazan string
-			if (s1.length() == 0 || s2.length() == 0) {
+            // ispituje da li je unet prazan string
+            if (s1.length() == 0 || s2.length() == 0) {
 
-				JOptionPane.showMessageDialog(null, "Popunite sva mesta");
-				continue;
+                JOptionPane.showMessageDialog(null, "Popunite sva mesta!");
+                continue;
 
-			}
-			// parsira unete vrednosti u int
-			try {
-				broj1 = Integer.parseInt(s1);
-				broj2 = Integer.parseInt(s2);
+            }
+            // proverava unete vrednosti da li je string double
+            pomocna1 = proveraBroj(s1);
+            pomocna2 = proveraBroj(s2);
 
-			} catch (NumberFormatException ex) {
-				JOptionPane.showMessageDialog(null, "Pogresan format, unesite ceo broj");
-				continue;
-			}
+            if (f == pomocna1 || f == pomocna2) {
+                JOptionPane.showMessageDialog(null, "Pogresan format, unesite ceo broj.");
+                continue;
+            }
+            //parsira string
+            broj1 = Double.parseDouble(s1);
+            broj2 = Double.parseDouble(s2);
 
-			String operacija = JOptionPane.showInputDialog("Unesite kod operacije: \r\n " + "Za sabiranje: s \r\n "
-					+ "Za oduzimanje: o \r\n" + "Za mnozenje dva broja: m \r\n" + "Za deljenje: d \r\n");
+            String operacija = JOptionPane.showInputDialog("Unesite kod operacije: \r\n " + "Za sabiranje: s \r\n "
+                    + "Za oduzimanje: o \r\n" + "Za mnozenje dva broja: m \r\n" + "Za deljenje: d \r\n");
 
-			switch (operacija) {
-			case "s":
+            switch (operacija) {
+                case "s":
+                    rezultat = broj1 + broj2;
+                    break;
+                case "o":
 
-				rezultat = broj1 + broj2;
-				break;
-			case "o":
+                    rezultat = broj1 - broj2;
+                    break;
+                case "m":
 
-				rezultat = broj1 - broj2;
-				break;
-			case "m":
+                    rezultat = broj1 * broj2;
+                    break;
+                case "d":
+                    if (broj2 != 0) {
+                        rezultat = broj1 / broj2;
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Nije dozvoljeno deljenje sa nulom!");
+                        continue;
+                    }
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Los unos opcije!");
+                    continue;
+            }
 
-				rezultat = broj1 * broj2;
-				break;
-			case "d":
-				if (broj2 != 0)
-					rezultat = broj1 / (double) broj2;
-				else
-					JOptionPane.showMessageDialog(null, "Nije dozvoljeno deljenje sa nulom!");
-				break;
-			default:
-				JOptionPane.showMessageDialog(null, "Los unos opcije!");
-				continue;
-			}
+            JOptionPane.showMessageDialog(null, "Resenje je: " + rezultat);
 
-			JOptionPane.showMessageDialog(null, "Resenje je: " + rezultat);
+            String nastavak = JOptionPane.showInputDialog("Da li zelite da nastavite sa programom (da ili ne)?");
+            if (nastavak.equalsIgnoreCase("da")) {
+            } else {
+                JOptionPane.showMessageDialog(null, "Kraj!");
+                break;
+            }
+        }
+    }
 
-			String nastavak = JOptionPane.showInputDialog("Da li zelite da nastavite sa programom (da ili ne)?");
-			if (nastavak.equalsIgnoreCase("da"))
-				continue;
-			else {
-				JOptionPane.showMessageDialog(null, "Kraj!");
-				break;
-			}
+    //metoda za proveru da li je string broj
+    public static boolean proveraBroj(String s) {
 
-		}
-
-	}
-
+        String regex = "[+-]?([0-9]*[.])?[0-9]+";
+        return s.matches(regex);
+    }
 }
