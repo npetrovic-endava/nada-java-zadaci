af737b3a37d7c421f18aa374b0e00b96debdd412
Nadezda Petrovic <nadezda.petrovic@endava.com>
2018-09-11 14:50:04 +0200

    izmena na zadatku

--- src/zadatak4/Student.java
+++ src/zadatak4/Student.java
@@ -1,82 +1,89 @@
+package zadatak4;
+
+import java.util.Scanner;
+
 /*
  * Program ispisuje ime studenta sa najvecim brojem poena na testu
  * prvo se unosi ukupan broj studenata, a zatim ime i broj poena svakog studenta
  */
-package zadatak4;
+
 /**
  *
  * @author Nadezda Petrovic
  */
-import java.util.Scanner;
 
 public class Student {
 
-    static Scanner ulaz = new Scanner(System.in);
+	static Scanner ulaz = new Scanner(System.in);
 
-    public static void main(String[] args) {
-    	
+	public static void main(String[] args) {
+
 //maxPoeni dodeljujemo vrednost 0 da bismo u prvom toku if naredbe imali vrednost maxPoeni= broj poena prvog studenta
-        int maxPoeni = 0;
-        StringBuilder sb = new StringBuilder("Najbolji student je: ");
-        String imeNajboljegStudenta = "";
-        String imeStudenta;
-        int poeni;
-        while (true) {
-            System.out.println("Uneti broj studenata:");
-            int n = proveraInt();
-            n = proveraBroj(n);
-            for (int i = 0; i < n; i++) {
-                System.out.println("Unesite ime studenta:");
-                imeStudenta = proveraString();
-                System.out.println("Unesite broj poena:");
 
-                poeni = proveraInt();
-                poeni = proveraBroj(poeni);
-                if (poeni > maxPoeni) {
-                    //dodeljujemo maxPoeni vrednost trenutnog najveceg  broja poena
-                    maxPoeni = poeni;
-                    imeNajboljegStudenta = imeStudenta;
-                }
-            }
-            System.out.println(sb.append(imeNajboljegStudenta));
+		int maxPoeni = 0;
+		String imeNajboljegStudenta = "";
+		String imeStudenta = "";
+		int poeni;
+		String komentar1 = "Unesite broj studenata:";
+		String komentar2 = "Unesite ime studenta:";
+		String komentar3 = "Unesite broj poena:";
+		String komentar4 = "Da li zelite da nastavite sa programom (da ili ne)?";
 
-            System.out.println("Da li zelite da nastavite sa programom (da ili ne)?");
-            String nastavak = ulaz.next();
-            if (nastavak.equalsIgnoreCase("da")) {
-                sb = new StringBuilder("Najbolji student je: ");
-            } else {
-                System.out.println("Kraj!");
-                break;
-            }
-        }
+		while (true) {
+			System.out.println(komentar1);
+			int n = proveraInt();
+			n = proveraBroj(n);
+			
+			for (int iterator = 0; iterator < n; iterator++) {
+				System.out.println(komentar2);
+				imeStudenta = proveraString();
+				System.out.println(komentar3);
 
-    }
+				poeni = proveraInt();
+				poeni = proveraBroj(poeni);
+				if (poeni > maxPoeni) {
+			// dodeljujemo maxPoeni vrednost trenutnog najveceg broja poena
+					maxPoeni = poeni;
+					imeNajboljegStudenta = imeStudenta;
+				}
+			}
+			System.out.println(new StringBuilder("Ime najboljeg studenta je ").append(imeNajboljegStudenta).toString());
 
-    public static String proveraString() {
-        String ime = "";
-        while (!ime.matches("^[a-zA-Z]+[\\-'\\s]?[a-zA-Z ]+$")) {
-            if (!ime.equals("")) {
-                System.out.print("Pogresan parametar, unesite ime studenta (String): ");
-            }
-            ime = ulaz.nextLine();
-        }
-        return ime;
-    }
+			System.out.println(komentar4);
+			String nastavak = ulaz.next();
+			if (!nastavak.equalsIgnoreCase("da")) {
+				System.out.println("Kraj!");
+				break;
+			}
+		}
 
-    public static int proveraInt() {
+	}
 
-        while (!ulaz.hasNextInt()) {
-            System.out.println("Pogresan parametar, unesite ceo broj:");
-            ulaz.next();
-        }
-        return ulaz.nextInt();
-    }
+	static String proveraString() {
+		String ime = "";
+		while (!ime.matches("^[a-zA-Z]+[\\-'\\s]?[a-zA-Z ]+$")) {
+			if (!ime.equals("")) {
+				System.out.print("Pogresan parametar, unesite ime studenta (String): ");
+			}
+			ime = ulaz.nextLine();
+		}
+		return ime;
+	}
 
-    public static int proveraBroj(int n) {
-        while (n <= 0) {
-            System.out.print("Pogresan parametar, unesite ceo broj veci od 0: ");
-            n = ulaz.nextInt();
-        }
-        return n;
-    }
+	static int proveraInt() {
+
+		while (!ulaz.hasNextInt()) {
+			System.out.println("Pogresan parametar, unesite ceo broj:");
+			ulaz.next();
+		}
+		return ulaz.nextInt();
+	}
+
+	static int proveraBroj(int n) {
+		while (0 >= n) {
+			System.out.print("Pogresan parametar, unesite ceo broj veci od 0: ");
+			n = ulaz.nextInt();
+		}
+		return n;
+	}
 }
