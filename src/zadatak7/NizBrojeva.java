b828c8263623b998c08894dd07e15ff871df98ea
Nadezda Petrovic <nadezda.petrovic@endava.com>
2018-09-12 10:24:20 +0200

    promena u zadatku niz brojeva

--- src/zadatak7/NizBrojeva.java
+++ src/zadatak7/NizBrojeva.java
@@ -1,35 +1,40 @@
-/**
- * Nakon unesenog niza od n brojeva, sortira niz, ispisuje da li sadrzi broj x
- * ispisuje prvih pet clanova niza
- * */
+
 package zadatak7;
- 
-/**
-*
-* @author Nadezda Petrovic
-*/
 
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * Nakon unesenog niza od n brojeva, sortira niz, ispisuje da li sadrzi broj x
+ * ispisuje prvih pet clanova niza
+ *
+ *
+ * @author Nadezda Petrovic
+ */
+
 public class NizBrojeva {
-	
-	static Scanner ulaz = new Scanner(System.in);
-	static int clanovi;
+
+	public static Scanner ulaz = new Scanner(System.in);
+	public static int clanovi;
 	public static int niz[];
+	public static final String UNOS_DUZINE_NIZA = "Uneti broj clanova niza veci ili jednak broju 5: ";
+	public static final String ISPIS_POGRESNOG_UNOSA = "Pogresan unos, broj treba da je veci ili jednak 5.";
+	public static final String ISPIS_NIZA="Dobijeni sortirani niz od pet clanova je:";
+	public static final String UNOS_BROJA_ZA_PROVERU="\nUneti broj za koji zelite da vidite da li je u nizu:";
+	public static final String ISPIS_PRIPADA="Broj pripada nizu.";
+	public static final String ISPIS_NE_PRIPADA="Broj ne pripada nizu.";
+	public static final String ISPIS_POGRESNOG_PARAMETRA="Pogresan parametar, unesite ceo broj veci od 5!";
 	
-
 	public static void main(String[] args) {
-
 		while (true) {
-			System.out.println("Uneti broj clanova niza veci ili jednak broju 5:");
-			//ispituje da li je unet ceo broj
+			System.out.println(UNOS_DUZINE_NIZA);
+			// ispituje da li je unet ceo broj
 			clanovi = proveraInt();
-			
-           //ispituje da li je unos broja clanova niza korektan
+
+			// ispituje da li je unos broja clanova niza korektan
 			if (5 > clanovi) {
-				
-				System.out.println("Pogresan unos, broj treba da je veci ili jednak 5");
+
+				System.out.println(ISPIS_POGRESNOG_UNOSA);
 			} else
 				break;
 		}
@@ -37,54 +42,41 @@ public class NizBrojeva {
 		niz = new int[clanovi];
 
 		for (int i = 0; i < clanovi; i++) {
-
-			System.out.println(new StringBuilder("Unesite ").append(i+1).append(". clan niza"));
-
+			System.out.println(new StringBuilder("Unesite ").append(i + 1).append(". clan niza"));
 			niz[i] = proveraInt();
-			
 		}
-		
-		
+
 		Arrays.sort(niz);
-		//ispisivanje prvih pet clanova sortiranog niza
-		
-		System.out.println("Dobijeni sortirani niz od pet clanova je:");
-		
+		// ispisivanje prvih pet clanova sortiranog niza
+
+		System.out.println(ISPIS_NIZA);
 		for (int i = 0; i < 5; i++) {
 			System.out.print(new StringBuilder().append(niz[i]).append(" "));
 		}
-		
-		
-		//provera da li je uneti broj clan niza
-		System.out.println("\nUneti broj za koji zelite da vidite da li je u nizu:");
-        
-		int brojX= proveraInt();
-		String pripada =brojPripadaNizu(brojX);
+
+		// provera da li je uneti broj clan niza
+		System.out.println(UNOS_BROJA_ZA_PROVERU);
+		int brojX = proveraInt();
+		String pripada = brojPripadaNizu(brojX);
 		System.out.println(pripada);
 		ulaz.close();
 	}
 
-	public static int proveraInt() {
-
+	private static int proveraInt() {
 		while (!ulaz.hasNextInt()) {
-
-			System.out.println("Pogresan parametar, unesite ceo broj veci od 5!");
-
+			System.out.println(ISPIS_POGRESNOG_PARAMETRA);
 			ulaz.next();
-
 		}
-
 		return ulaz.nextInt();
-
 	}
-	public static String brojPripadaNizu(int x) {
-		for(int i=0; i<clanovi; i++) {
-			if(x==niz[i]) {
-				return "Broj pripada nizu.";
+
+	private static String brojPripadaNizu(int x) {
+		boolean pripadaNizu=false;
+		for (int i = 0; i < clanovi; i++) {
+			if (x == niz[i]) {
+				pripadaNizu=true;
 			}
 		}
-				return "Broj ne pripada nizu.";
-		
-
+		return pripadaNizu?ISPIS_PRIPADA:ISPIS_NE_PRIPADA;
 	}
 }
