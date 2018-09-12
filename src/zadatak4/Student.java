70dee15d1448fa1103d4f1b9dc2eeac04eee06cc
Nadezda Petrovic <nadezda.petrovic@endava.com>
2018-09-12 10:02:11 +0200

    ispravka zadatkda student

--- src/zadatak4/Student.java
+++ src/zadatak4/Student.java
@@ -5,58 +5,56 @@ import java.util.Scanner;
 /*
  * Program ispisuje ime studenta sa najvecim brojem poena na testu
  * prvo se unosi ukupan broj studenata, a zatim ime i broj poena svakog studenta
- */
-
-/**
  *
  * @author Nadezda Petrovic
  */
-
 public class Student {
 
-	static Scanner ulaz = new Scanner(System.in);
-
+	private static Scanner ulaz = new Scanner(System.in);
+	public static final String UNOS_BROJA_STUDENATA = "Unesite broj studenata: ";
+	public static final String UNOS_IMENA_STUDENATA = "Unesite ime studenta: ";
+	public static final String UNOS_BROJA_POENA = "Unesite broj poena:";
+	public static final String NASTAVAK_PROGRAMA = "Da li zelite da nastavite sa programom (da ili ne)?";
+	public static final String NAJBOLJI_STUDENT_ISPIS = "Ime najboljeg studenta je ";
+	public static final String KRAJ_PROGRAMA = "Kraj";
 	public static void main(String[] args) {
 
-//maxPoeni dodeljujemo vrednost 0 da bismo u prvom toku if naredbe imali vrednost maxPoeni= broj poena prvog studenta
+         //maxPoeni dodeljujemo vrednost 0 da bismo u prvom toku if
+		// naredbe imali vrednost maxPoeni= broj poena prvog studenta
 
 		int maxPoeni = 0;
-		String imeNajboljegStudenta = "";
-		String imeStudenta = "";
+		String imeNajboljegStudenta = null;
+		String imeStudenta = null;
 		int poeni;
-		String komentar1 = "Unesite broj studenata:";
-		String komentar2 = "Unesite ime studenta:";
-		String komentar3 = "Unesite broj poena:";
-		String komentar4 = "Da li zelite da nastavite sa programom (da ili ne)?";
 
 		while (true) {
-			System.out.println(komentar1);
+			System.out.println(UNOS_BROJA_STUDENATA);
 			int n = proveraInt();
 			n = proveraBroj(n);
-			
+
 			for (int iterator = 0; iterator < n; iterator++) {
-				System.out.println(komentar2);
+				System.out.println(UNOS_IMENA_STUDENATA);
 				imeStudenta = proveraString();
-				System.out.println(komentar3);
+				System.out.println(UNOS_BROJA_POENA);
 
 				poeni = proveraInt();
 				poeni = proveraBroj(poeni);
 				if (poeni > maxPoeni) {
-			// dodeljujemo maxPoeni vrednost trenutnog najveceg broja poena
+					// dodeljujemo maxPoeni vrednost trenutnog najveceg broja poena
 					maxPoeni = poeni;
 					imeNajboljegStudenta = imeStudenta;
 				}
 			}
-			System.out.println(new StringBuilder("Ime najboljeg studenta je ").append(imeNajboljegStudenta).toString());
+			System.out.println(new StringBuilder(NAJBOLJI_STUDENT_ISPIS).append(imeNajboljegStudenta).toString());
 
-			System.out.println(komentar4);
+			System.out.println(NASTAVAK_PROGRAMA);
 			String nastavak = ulaz.next();
 			if (!nastavak.equalsIgnoreCase("da")) {
-				System.out.println("Kraj!");
+				System.out.println(KRAJ_PROGRAMA);
 				break;
 			}
 		}
-
+		ulaz.close();
 	}
 
 	static String proveraString() {
