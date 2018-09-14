9fe7c78652b0f585bd0cf560da39dc35c5dcbe55
Nadezda Petrovic <nadezda.petrovic@endava.com>
2018-09-14 16:36:24 +0200

    izmena na zadatku student

--- src/zadatak4/Student.java
+++ src/zadatak4/Student.java
@@ -9,37 +9,32 @@ import java.util.Scanner;
  * @author Nadezda Petrovic
  */
 public class Student {
-
 	private static Scanner ulaz = new Scanner(System.in);
-	public static final String UNOS_BROJA_STUDENATA = "Unesite broj studenata: ";
-	public static final String UNOS_IMENA_STUDENATA = "Unesite ime studenta: ";
-	public static final String UNOS_BROJA_POENA = "Unesite broj poena:";
-	public static final String NASTAVAK_PROGRAMA = "Da li zelite da nastavite sa programom (da ili ne)?";
-	public static final String NAJBOLJI_STUDENT_ISPIS = "Ime najboljeg studenta je ";
-	public static final String KRAJ_PROGRAMA = "Kraj";
-	public static final String ISPIS_ZA_POGRESAN_PARAMETAR_STRING = "Pogresan parametar, unesite ime studenta (String): ";
-	public static final String ISPIS_ZA_POGRESAN_PARAMETAR_INT = "Pogresan parametar, unesite ceo broj veci od 0:";
+	private static final String UNOS_BROJA_STUDENATA = "Unesite broj studenata: ";
+	private static final String UNOS_IMENA_STUDENATA = "Unesite ime studenta: ";
+	private static final String UNOS_BROJA_POENA = "Unesite broj poena:";
+	private static final String NASTAVAK_PROGRAMA = "Da li zelite da nastavite sa programom (da ili ne)?";
+	private static final String NAJBOLJI_STUDENT_ISPIS = "Ime najboljeg studenta je ";
+	private static final String KRAJ_PROGRAMA = "Kraj";
+	private static final String ISPIS_ZA_POGRESAN_PARAMETAR_STRING = "Pogresan parametar, unesite ime studenta (String): ";
+	private static final String ISPIS_ZA_POGRESAN_PARAMETAR_INT = "Pogresan parametar, unesite ceo broj veci od 0:";
+	private static final String REGEX = "^[a-zA-Z]+[\\-'\\s]?[a-zA-Z ]+$";
 
 	public static void main(String[] args) {
-
 		// maxPoeni dodeljujemo vrednost 0 da bismo u prvom toku if
 		// naredbe imali vrednost maxPoeni= broj poena prvog studenta
-
 		int maxPoeni = 0;
 		String imeNajboljegStudenta = null;
 		String imeStudenta = null;
 		int poeni;
-
 		while (true) {
 			System.out.println(UNOS_BROJA_STUDENATA);
 			int n = proveraInt();
 			n = proveraBroj(n);
-
 			for (int iterator = 0; iterator < n; iterator++) {
 				System.out.println(UNOS_IMENA_STUDENATA);
 				imeStudenta = proveraString();
 				System.out.println(UNOS_BROJA_POENA);
-
 				poeni = proveraInt();
 				poeni = proveraBroj(poeni);
 				if (poeni > maxPoeni) {
@@ -60,9 +55,9 @@ public class Student {
 		ulaz.close();
 	}
 
-	static String proveraString() {
+	private static String proveraString() {
 		String ime = "";
-		while (!ime.matches("^[a-zA-Z]+[\\-'\\s]?[a-zA-Z ]+$")) {
+		while (!ime.matches(REGEX)) {
 			if (!ime.equals("")) {
 				System.out.print(ISPIS_ZA_POGRESAN_PARAMETAR_STRING);
 			}
@@ -71,7 +66,7 @@ public class Student {
 		return ime;
 	}
 
-	static int proveraInt() {
+	private static int proveraInt() {
 
 		while (!ulaz.hasNextInt()) {
 			System.out.println(ISPIS_ZA_POGRESAN_PARAMETAR_INT);
@@ -80,7 +75,7 @@ public class Student {
 		return ulaz.nextInt();
 	}
 
-	static int proveraBroj(int n) {
+	private static int proveraBroj(int n) {
 		while (0 >= n) {
 			System.out.print(ISPIS_ZA_POGRESAN_PARAMETAR_INT);
 			n = ulaz.nextInt();
